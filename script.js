61017ddcffccc0c1151c466707aaa356f69b15d0
annauzun <terziann@mail.ru>
2023-06-22 17:44:14 -0500

    html and styles created

---
+++ script.js
@@ -0,0 +1,50 @@
+const books = [
+    {
+        title: 'В стране невыученных уроков',
+        authors: 'Гераскина Лия Борисовна',
+        editor: 'Самовар',
+        year: '2013',
+        image: 'https://img3.labirint.ru/rc/02e128e496e28abfc08ac859a8e78d84/363x561q80/books4/32715/cover.jpg?1370355905'
+    },
+ 
+    {
+        title: 'Желтый туман',
+        authors: 'Волков Александр Мелентьевич',
+        editor: 'Самовар',
+        year: '2014',
+        image: 'https://img3.labirint.ru/rc/b8a6d5ffb869ee7f9a002fdd1667cc37/363x561q80/books24/238633/cover.jpg?1280394613'
+    },
+ 
+    {
+        title: 'Живая шляпа и другие любимые рассказы',
+        authors: 'Носов Николай Николаевич',
+        editor: 'Махаон',
+        year: '2022',
+        image: 'https://img3.labirint.ru/rc/44e17656f0668521f8a23c6d1dfb2a62/363x561q80/books92/911551/cover.jpg?1682090795'
+    },
+ 
+    {
+        title: 'Денискины рассказы',
+        authors: 'Драгунский Виктор Юзефович',
+        year: '2014',
+        editor: 'Самовар',
+        image: 'https://img4.labirint.ru/rc/bbe422f59a0eff1da98ff85eaf3fcdde/363x561q80/books21/207572/cover.jpg?1280394613'
+    },
+ 
+    {
+        title: 'Медвежонок Паддингтон и его невероятные приключения',
+        authors: 'Бонд Майкл',
+        editor: 'Азбука',
+        year: '2022',
+        image: 'https://img3.labirint.ru/rc/d8ccd154d31ae06851cfae5dfeb10866/363x561q80/books49/482941/cover.jpg?1670138704'
+    },
+ 
+    {
+        title: 'Приключения Незнайки и его друзей',
+        authors: 'Носов Николай Николаевич',
+        editor: 'Махаон',
+        year: '2022',
+        image: 'https://img3.labirint.ru/rc/b8a6d5ffb869ee7f9a002fdd1667cc37/363x561q80/books24/238633/cover.jpg?1280394613'
+    }
+ 
+]
